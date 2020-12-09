73621d4bbc27947d55fc9d069f2830f424166109
11758 <74574770+11758@users.noreply.github.com>
2020-12-09 09:39:07 +0800

    Update StudentModel.java

--- student_prc/src/com/student/model/StudentModel.java
+++ student_prc/src/com/student/model/StudentModel.java
@@ -16,13 +16,14 @@ public class StudentModel {
 		this .love=love;
 		
 		
-		[branch "master"] 
+	[branch "master"] 
 	remote = origin 
 	merge = refs/heads/master
-        [remote "origin"] 
-	url = https://github.com/huangbangyi2019/git_project.git 
+[remote "origin"] 
+	url = https://github.com/11758/git_project.git
 	fetch = +refs/heads/*:refs/remotes/origin/* 
 	push = refs/heads/master:refs/heads/master
+
 
 		
 	}
